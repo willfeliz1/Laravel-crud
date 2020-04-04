e8752210f72aa6872bf23d5d11b279e5c3f4e09f
willfeliz1 <william_mfelizardo@hotmail.com>
2020-04-04 20:07:40 -0300

    user controller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('users/new', 'UsersController@store');
+Route::get('/login','UsersController@index');
+Route::put('users/{id}/update', 'UsersController@update');
+Route::get('users/{id}/delete', 'UsersController@delete');
+
+
